d3bbedff4170a453427588d5b4e63344b0ab08d8
Samuel Sunko <ssunko60@stuy.edu>
2024-05-28 14:50:41 -0400

    + w38 (usingsome html)

--- py/w38_rand.py
+++ py/w38_rand.py
@@ -4,4 +4,10 @@ print('Content-type: text/html\n')
 
 from random import random
 r=random()
+print('''<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>SSUNKO Index</title>
+  </head>''')
 print('a random number for you:', r)
